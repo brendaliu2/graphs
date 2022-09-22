0f461229dacb13e5b8cd75e92a6cc4ac3e9c08a8
Aishwarya Majumdar <aishu.maj1996@gmail.com>
2022-09-22 12:24:13 -0700

    completed graph class

--- graph.js
+++ graph.js
@@ -57,13 +57,66 @@ class Graph {
   }
 
   /** traverse graph with DFS and returns array of Node values */
-  depthFirstSearch(start) { }
+  depthFirstSearch(start) { 
+    //create toVisitStack = [n1]
+    let toVisitStack = [start];
+    //new set of visited neighbors : visited = new Set()
+    let visitedNodes = new Set();
+    
+    //while loop: as long as stack has a length
+    while(toVisitStack.length){
+      //neighbor = pop stack
+      let current = toVisitStack.pop();
+      //if !visited.has(neighbor) -> add to visited
+      if(!visitedNodes.has(current.value)) {
+        visitedNodes.add(current.value);
+        //look thru neighbors adjacent, push neighbors to stack
+        for(let neighbor of current.adjacent) toVisitStack.push(neighbor);
+      }
+    }
+    return Array.from(visitedNodes);
+  }
 
   /** traverse graph with BDS and returns array of Node values */
-  breadthFirstSearch(start) { }
+  breadthFirstSearch(start) { 
+    //toVisitQueue = [start]
+    let toVisitQueue = [start];
+    //new set of visited neighbors: visited = new Set();
+    let visited = new Set();
+    
+    //while loop: Queue has length
+    while(toVisitQueue.length){
+      let current = toVisitQueue.shift();
+       
+      if(!visited.has(current.value)){
+        visited.add(current.value);
+        
+        for(let neighbor of current.adjacent) toVisitQueue.push(neighbor);
+      }
+    }   
+    return Array.from(visited);
+  }
 
   /** find the distance of the shortest path from the start vertex to the end vertex */
-  distanceOfShortestPath(start, end) { }
+  distanceOfShortestPath(start, end) {
+    //toVisitQueue = [start]
+    let layer = 0;
+    let toVisitQueue = [[start, layer]];
+    //new set of visited neighbors: visited = new Set();
+    let visited = new Set();
+    
+    while(toVisitQueue.length){
+      let [current, layer] = toVisitQueue.shift();
+      
+      if(current === end) return layer;
+      
+      if(!visited.has(current.value)){
+        visited.add(current.value);
+        
+        for(let neighbor of current.adjacent) toVisitQueue.push([neighbor, layer+1]);
+      }
+    }   
+   }
 }
 
 module.exports = { Graph, Node };
